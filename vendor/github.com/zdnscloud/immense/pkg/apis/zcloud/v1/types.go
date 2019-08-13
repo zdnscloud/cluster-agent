00042380c71be554e79ce86e7471469064d94551
thesinglecloud <hanfeng@zdns.cn>
2019-08-13 09:04:54 +0000

    update vender

--- vendor/github.com/zdnscloud/immense/pkg/apis/zcloud/v1/types.go
+++ vendor/github.com/zdnscloud/immense/pkg/apis/zcloud/v1/types.go
@@ -13,9 +13,8 @@ type ClusterSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
-	StorageType string `json:"storageType"`
-	//Hosts       []HostSpec `json:"hosts"`
-	Hosts []string `json:"hosts"`
+	StorageType string     `json:"storageType"`
+	Hosts       []HostSpec `json:"hosts"`
 }
 
 type HostSpec struct {
