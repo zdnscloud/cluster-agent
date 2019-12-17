1f0f83dbbbd6dd387ec00203f7128ef912aae679
joe-yu <yuhaijiao@zdns.cn>
2019-12-17 09:15:06 +0000

    add commonresource

---
+++ commonresource/types.go
@@ -0,0 +1,37 @@
+package commonresource
+
+import (
+	"github.com/zdnscloud/gorest/resource"
+)
+
+type Namespace struct {
+	resource.ResourceBase `json:",inline"`
+	Name                  string `json:"name,omitempty"`
+}
+
+type Deployment struct {
+	resource.ResourceBase `json:",inline"`
+	Name                  string `json:"name,omitempty"`
+}
+
+func (d Deployment) GetParents() []resource.ResourceKind {
+	return []resource.ResourceKind{Namespace{}}
+}
+
+type DaemonSet struct {
+	resource.ResourceBase `json:",inline"`
+	Name                  string `json:"name,omitempty"`
+}
+
+func (d DaemonSet) GetParents() []resource.ResourceKind {
+	return []resource.ResourceKind{Namespace{}}
+}
+
+type StatefulSet struct {
+	resource.ResourceBase `json:",inline"`
+	Name                  string `json:"name,omitempty"`
+}
+
+func (s StatefulSet) GetParents() []resource.ResourceKind {
+	return []resource.ResourceKind{Namespace{}}
+}
